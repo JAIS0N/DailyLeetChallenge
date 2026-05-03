723c56f08eb8f4d2e97c76d1c03a10d33f45ddc6
Jaison Menezes <menezesjaison@gmail.com>
2026-05-03 13:46:06 -0700

    Time: 8 ms (63.26%), Space: 19.7 MB (8.16%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.py
@@ -0,0 +1,36 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        col = set()
+        posDiag = set()  # (r + c) is constant on each ↗ diagonal
+        negDiag = set()  # (r - c) is constant on each ↘ diagonal
+
+        res = []
+        board = [["."] * n for i in range(n)]
+
+        def backtrack(r):
+            if r == n:
+                copy = ["".join(row) for row in board]
+                res.append(copy)
+                return
+
+            for c in range(n):
+                if c in col or (r + c) in posDiag or (r - c) in negDiag:
+                    continue
+
+                col.add(c)
+                posDiag.add(r + c)
+                negDiag.add(r - c)
+                board[r][c] = "Q"
+
+                backtrack(r + 1)
+
+                col.remove(c)
+                posDiag.remove(r + c)
+                negDiag.remove(r - c)
+                board[r][c] = "."
+            
+
+        backtrack(0)
+        count = sum(1 for item in res if isinstance(item, list))
+        return count
+        
